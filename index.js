efda408c4ad43173910608728ed2a679393743c5
junayet <muhammadjunayetmaruf@gmail.com>
2023-11-25 19:10:56 +0600

    jwt has implemented

--- index.js
+++ index.js
@@ -28,6 +28,33 @@ async function run() {
         const upcommingCampCollection = mediserveMobilize.collection('upcommingCamp')
         const usersCollection = mediserveMobilize.collection('users');
         const registrationCampCollection = mediserveMobilize.collection('registrationCamp');
+
+        // jwt related api start
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ token })
+        })
+        // jwt related api end
+        // middlware start
+        const verifyToken = (req, res, next) => {
+            console.log('inside vefified token', req.headers.authorization);
+            if (!req?.headers?.authorization) {
+                return res.status(401).send({ message: 'unauthrised' })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthrised' })
+                }
+                req.decoded = decoded
+                next()
+            })
+        }
+        // middlware end
+
         // usersCollection start
         app.post('/users', async (req, res) => {
             const user = req.body;
